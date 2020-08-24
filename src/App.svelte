aee6390aff86eff2f4d84ef4060c9dc01ac9298c
pacificescape <17690123+pacificescape@users.noreply.github.com>
2020-08-24 06:30:38 +0200

    noselect

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,10 @@
 		background-color: #2d2d2d;
 		text-align: center;
 		margin: 0 auto;
-		user-select: none;
+		-webkit-user-select: none;
+		   -moz-user-select: none;
+		    -ms-user-select: none;
+		        user-select: none;
 	}
 
 	@media (min-width: 640px) {
